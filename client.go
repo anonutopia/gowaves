0ff4d8eb4d97d747062d7685175370f9f9490294
Pragmatic <cryptopragmatic@protonmail.com>
2022-06-12 22:33:34 +0200

    fix

--- client.go
+++ client.go
@@ -29,7 +29,7 @@ func initWNC() *WavesNodeClient {
 	return wnc
 }
 
-func (w *WavesNodeClient) DoRequest(uri string, method string, body interface{}, resp interface{}) error {
+func (w *WavesNodeClient) DoRequest(uri string, method string, body interface{}, resp interface{}, auth bool) error {
 	cl := http.Client{}
 	var url string
 
@@ -63,7 +63,9 @@ func (w *WavesNodeClient) DoRequest(uri string, method string, body interface{},
 		}
 	}
 
-	req.Header.Set("X-API-Key", w.ApiKey)
+	if auth {
+		req.Header.Set("X-API-Key", w.ApiKey)
+	}
 	req.Header.Set("Content-Type", "application/json")
 
 	if err != nil {
@@ -94,85 +96,85 @@ func (w *WavesNodeClient) DoRequest(uri string, method string, body interface{},
 
 func (w *WavesNodeClient) ActivationStatus() (*ActivationStatusResponse, error) {
 	asr := &ActivationStatusResponse{}
-	err := w.DoRequest("/activation/status", http.MethodGet, nil, asr)
+	err := w.DoRequest("/activation/status", http.MethodGet, nil, asr, false)
 	return asr, err
 }
 
 func (w *WavesNodeClient) NodeVersion() (*NodeVersionResponse, error) {
 	nvr := &NodeVersionResponse{}
-	err := w.DoRequest("/node/version", http.MethodGet, nil, nvr)
+	err := w.DoRequest("/node/version", http.MethodGet, nil, nvr, false)
 	return nvr, err
 }
 
 func (w *WavesNodeClient) NodeStop() (*NodeStopResponse, error) {
 	nvr := &NodeStopResponse{}
-	err := w.DoRequest("/node/stop", http.MethodPost, nil, nvr)
+	err := w.DoRequest("/node/stop", http.MethodPost, nil, nvr, true)
 	return nvr, err
 }
 
 func (w *WavesNodeClient) NodeStatus() (*NodeStatusResponse, error) {
 	nsr := &NodeStatusResponse{}
-	err := w.DoRequest("/node/status", http.MethodGet, nil, nsr)
+	err := w.DoRequest("/node/status", http.MethodGet, nil, nsr, false)
 	return nsr, err
 }
 
 func (w *WavesNodeClient) AddressValidate(address string) (*AddressValidateResponse, error) {
 	avr := &AddressValidateResponse{}
-	err := w.DoRequest(fmt.Sprintf("/addresses/validate/%s", address), http.MethodGet, nil, avr)
+	err := w.DoRequest(fmt.Sprintf("/addresses/validate/%s", address), http.MethodGet, nil, avr, false)
 	return avr, err
 }
 
 func (w *WavesNodeClient) Addresses() (*AddressesResponse, error) {
 	ar := &AddressesResponse{}
-	err := w.DoRequest("/addresses", http.MethodGet, nil, ar)
+	err := w.DoRequest("/addresses", http.MethodGet, nil, ar, false)
 	return ar, err
 }
 
 func (w *WavesNodeClient) AddressesBalance(address string) (*AddressesBalanceResponse, error) {
 	abr := &AddressesBalanceResponse{}
-	err := w.DoRequest(fmt.Sprintf("/addresses/balance/%s", address), http.MethodGet, nil, abr)
+	err := w.DoRequest(fmt.Sprintf("/addresses/balance/%s", address), http.MethodGet, nil, abr, false)
 	return abr, err
 }
 
 func (w *WavesNodeClient) AssetsTransfer(atr *AssetsTransferRequest) (*AssetsTransferResponse, error) {
 	atres := &AssetsTransferResponse{}
-	err := w.DoRequest("/assets/transfer", http.MethodPost, atr, atres)
+	err := w.DoRequest("/assets/transfer", http.MethodPost, atr, atres, true)
 	return atres, err
 }
 
 func (w *WavesNodeClient) AssetsMassTransfer(amtr *AssetsMassTransferRequest) (*AssetsMassTransferResponse, error) {
 	amtres := &AssetsMassTransferResponse{}
-	err := w.DoRequest("/assets/masstransfer", http.MethodPost, amtr, amtres)
+	err := w.DoRequest("/assets/masstransfer", http.MethodPost, amtr, amtres, true)
 	return amtres, err
 }
 
 func (w *WavesNodeClient) AssetsOrder(aor *AssetsOrderRequest) (*AssetsOrderResponse, error) {
 	aors := &AssetsOrderResponse{}
-	err := w.DoRequest("/assets/order", http.MethodPost, aor, aors)
+	err := w.DoRequest("/assets/order", http.MethodPost, aor, aors, true)
 	return aors, err
 }
 
 func (w *WavesNodeClient) BlocksAt(n uint) (*BlocksAtResponse, error) {
 	bar := &BlocksAtResponse{}
-	err := w.DoRequest(fmt.Sprintf("/blocks/at/%d", n), http.MethodGet, nil, bar)
+	err := w.DoRequest(fmt.Sprintf("/blocks/at/%d", n), http.MethodGet, nil, bar, false)
 	return bar, err
 }
 
 func (w *WavesNodeClient) BlocksHeight() (*BlocksHeightResponse, error) {
 	bhr := &BlocksHeightResponse{}
-	err := w.DoRequest("/blocks/height", http.MethodGet, nil, bhr)
+	err := w.DoRequest("/blocks/height", http.MethodGet, nil, bhr, false)
 	return bhr, err
 }
 
 func (w *WavesNodeClient) TransactionsAddressLimit(address string, limit uint) ([][]TransactionsAddressLimitResponse, error) {
 	var talr [][]TransactionsAddressLimitResponse
-	err := w.DoRequest(fmt.Sprintf("/transactions/address/%s/limit/%d", address, limit), http.MethodGet, nil, &talr)
+	err := w.DoRequest(fmt.Sprintf("/transactions/address/%s/limit/%d", address, limit), http.MethodGet, nil, &talr, false)
 	return talr, err
 }
 
 func (w *WavesNodeClient) AssetsBalance(address string, assetId string) (*AssetsBalanceResponse, error) {
 	abr := &AssetsBalanceResponse{}
-	err := w.DoRequest(fmt.Sprintf("/assets/balance/%s/%s", address, assetId), http.MethodGet, nil, abr)
+	err := w.DoRequest(fmt.Sprintf("/assets/balance/%s/%s", address, assetId), http.MethodGet, nil, abr, false)
 	return abr, err
 }
 
@@ -182,7 +184,7 @@ func (w *WavesNodeClient) AssetsBalanceDistribution(assetId string, height int, 
 	if len(after) > 0 {
 		url = fmt.Sprintf("%s?after=%s", url, after)
 	}
-	err := w.DoRequest(url, http.MethodGet, nil, abdr)
+	err := w.DoRequest(url, http.MethodGet, nil, abdr, false)
 	return abdr, err
 }
 
@@ -192,12 +194,12 @@ func (w *WavesNodeClient) AddressesData(address string, key string) (*DataRespon
 	if len(key) > 0 {
 		ur += fmt.Sprintf("?matches=%s", url.QueryEscape(key))
 	}
-	err := w.DoRequest(ur, http.MethodGet, nil, adr)
+	err := w.DoRequest(ur, http.MethodGet, nil, adr, false)
 	return adr, err
 }
 
 func (w *WavesNodeClient) UtilsHashSecure(message string) (*UtilsHashSecureResponse, error) {
 	uhsr := &UtilsHashSecureResponse{}
-	err := w.DoRequest("/assets/masstransfer", http.MethodPost, message, uhsr)
+	err := w.DoRequest("/assets/masstransfer", http.MethodPost, message, uhsr, false)
 	return uhsr, err
 }
